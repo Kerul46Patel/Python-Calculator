6c388190d8bc1afbfe784c3005a38393945a8d66
Kerul46Patel <kerulfaldu@gmail.com>
2024-11-06 12:21:15 +0530

    Add files via upload

---
+++ Calculator_PYTHON.py
@@ -0,0 +1,26 @@
+
+# Userinput Value store variable
+a = int(input("Enter First Values : "))
+b = int(input("Enter Second Values : "))
+
+print("--------------------------------------")
+
+# Userinput Operator store variable
+c = str(input("Enter Arithmetic Operator : "))
+
+print("--------------------------------------")
+
+# Perfomance Operation
+if(c == "+"):
+    print("Ans Is : ",a+b)
+elif(c == "-"):
+    print("Ans IS : ",a-b)
+elif(c == "*"):
+    print("Ans IS : ",a*b)
+elif(c == "/"):
+    print("Ans IS : ",a/b)
+else:
+    print("Please Valid Operator Enter...")
+
+# Thanks For Watching
+# Like, Follow, Subscribe for code world Chanel
